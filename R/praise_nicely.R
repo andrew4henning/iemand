5e4624363e779aad24578c79e5b9bd4e50f98929
Andrew <andrew4henning@me.com>
2018-03-16 14:49:36 +0200

    take upper up

--- R/praise_nicely.R
+++ R/praise_nicely.R
@@ -5,6 +5,6 @@
 #' @return a praise (character)
 #' @export
  praise_nicely = function(name){
-     toupper( praise::praise(template = paste0(name, " is ${adjective}")))
+   praise::praise(template = paste0(name, " is ${adjective}"))
 
   }
